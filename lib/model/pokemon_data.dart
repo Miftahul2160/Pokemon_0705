558266b1d513879248247e35e1775c12944848f6
Miftahul2160 <miftahulum2160@gmail.com>
2025-12-13 14:14:13 +0700

    commit pokemon data

---
+++ lib/model/pokemon_data.dart
@@ -0,0 +1,12 @@
+class PokemonData {
+  final int id;
+  final String name;
+  final String imageUrl;
+  final String type;
+  PokemonData({
+    required this.id,
+    required this.name,
+    required this.imageUrl,
+    required this.type,
+  });
+}
