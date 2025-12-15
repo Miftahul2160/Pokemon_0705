eb0e190367964d158bcbc236cb1cd889826b06f9
Miftahul2160 <miftahulum2160@gmail.com>
2025-12-15 17:11:44 +0700

    commit revisi build card pokemon

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -34,7 +34,7 @@ class _HomePageState extends State<HomePage> {
               Text(
                 'Search for a pokemon by name or using its National Pokedex number.',
                 style: TextStyle(fontSize: 20, color: Colors.blueAccent),
-                ),
+              ),
               SizedBox(height: 20),
               TextField(
                 controller: searchNameController,
@@ -48,39 +48,44 @@ class _HomePageState extends State<HomePage> {
               ),
               SizedBox(height: 20),
               Expanded(
-                child: 
-                GridView.builder(
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    crossAxisSpacing: 10,
-                    mainAxisSpacing: 10,
-                    ), 
-                  itemCount: dummyPokemonsList.length,  
-                  itemBuilder: (context, index) {
-                    final pokemon =  dummyPokemonsList[index];
-                    return listPokemon(context, pokemon);
-                  }
-                    )
-
-                    )
-              ],
+                  child: GridView.builder(
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      crossAxisSpacing: 10,
+                      mainAxisSpacing: 8,
+                      childAspectRatio: 3 / 4,
+                    ),
+                    itemCount: dummyPokemonsList.length,
+                    itemBuilder: (context, index) {
+                      final pokemon = dummyPokemonsList[index];
+                      return listPokemon(context, pokemon);
+                    },
+                  
+              ),
+              )
+            ],
           ),
         ),
       ),
     );
   }
-  
+
   listPokemon(BuildContext context, PokemonData pokemon) {
     return GestureDetector(
       onTap: () {
         Navigator.push(
           context,
-          MaterialPageRoute(
-            builder: (context) => DetailCard(pokemon: pokemon),
-          ),
+          MaterialPageRoute(builder: (context) => DetailCard(pokemon: pokemon)),
         );
       },
       child: Card(
+        color: pokemon.type == 'Fire'
+            ? Colors.redAccent.shade100
+            : pokemon.type == 'Water'
+            ? Colors.blueAccent.shade100
+            : pokemon.type == 'Grass'
+            ? Colors.greenAccent.shade100
+            : Colors.grey.shade100,
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(12.0),
         ),
@@ -90,18 +95,20 @@ class _HomePageState extends State<HomePage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Image.network(
-                pokemon.imageUrl,
-                height: 100,
-                fit: BoxFit.cover,
-              ),
+              Image.network(pokemon.imageUrl, height: 100, fit: BoxFit.cover),
               SizedBox(height: 10),
               Text(
                 pokemon.name,
                 style: TextStyle(
                   fontSize: 18,
                   fontWeight: FontWeight.bold,
+                  color: Colors.blueAccent,
                 ),
+              ),
+              SizedBox(height: 10),
+              Text(
+                pokemon.id.toString(),
+                style: TextStyle(fontSize: 18, color: Colors.blueAccent),
               ),
             ],
           ),
@@ -109,6 +116,4 @@ class _HomePageState extends State<HomePage> {
       ),
     );
   }
-
-  
 }
